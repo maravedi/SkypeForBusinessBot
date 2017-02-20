0eb153cfdf720e84c59a5be038cc220755fa5dd5
David Frazer <capturemystery@gmail.com>
2017-02-20 14:37:28 -0500

    Added some more responses

--- LyncBot.Core/Dialogs/LyncLuisDialog.cs
+++ LyncBot.Core/Dialogs/LyncLuisDialog.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace LyncBot.Core.Dialogs
 {
-    [LuisModel("model id", "subs key")]
+    [LuisModel("3a91f550-4ed6-4628-adad-84f008eb9bfc", "9fad47d8a341461fbf07b0dfdf25c945")]
     [Serializable]
     public class LyncLuisDialog : LuisDialog<object>
     {
@@ -20,20 +20,47 @@ namespace LyncBot.Core.Dialogs
         {
             _presenceService = presenceService;
         }
-        [LuisIntent("")]
+        [LuisIntent("None")]
         public async Task None(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
         {
-            //await context.PostAsync("I'm sorry. I didn't understand you.");
+            await context.PostAsync("I'm sorry. I didn't understand you.");
             // Dont do anything. Pretend I am busy.
             context.Wait(MessageReceived);
         }
 
-        [LuisIntent("HiGreetings")]
-        public async Task HiGreetings(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        [LuisIntent("WelcomeGreetings")]
+        public async Task WelcomeGreetings(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
         {
             var activity = await message;
             string name = GetName(activity.From);
-            await context.PostOnlyOnceAsync(Responses.HiGreetingsResponse(name), nameof(HiGreetings));
+            await context.PostOnlyOnceAsync(Responses.WelcomeGreetingsResponse(name), nameof(WelcomeGreetings));
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("PasswordReset")]
+        public async Task PasswordReset(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            //string name = string.Empty;
+            //if (!context.PrivateConversationData.ContainsKey(nameof(WelcomeGreetings)))
+            //{
+            var activity = await message;
+            string name = GetName(activity.From);
+            //}
+            await context.PostOnlyOnceAsync(Responses.PasswordResetResponse(name), nameof(PasswordReset));
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("UnlockAccount")]
+        public async Task UnlockAccount(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            //string name = string.Empty;
+            //if (!context.PrivateConversationData.ContainsKey(nameof(WelcomeGreetings)))
+            //{
+            var activity = await message;
+            string name = activity.From.Id;
+            string text = activity.Text;
+            //}
+            await context.PostOnlyOnceAsync(Responses.UnlockAccountResponse(name, text), nameof(UnlockAccount));
             context.Wait(MessageReceived);
         }
 
@@ -41,7 +68,7 @@ namespace LyncBot.Core.Dialogs
         public async Task GoodMorningGreetings(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
         {
             string name = string.Empty;
-            if (!context.PrivateConversationData.ContainsKey(nameof(HiGreetings)))
+            if (!context.PrivateConversationData.ContainsKey(nameof(WelcomeGreetings)))
             {
                 var activity = await message;
                 name = GetName(activity.From);
@@ -75,5 +102,7 @@ namespace LyncBot.Core.Dialogs
             }
             return name;
         }
+
     }
+
 }
--- LyncBot.Core/Responses.cs
+++ LyncBot.Core/Responses.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LyncBot.Core
 {
     public static class Responses
     {
-        public static List<string> HiGreetingsResponse(string name)
+        public static List<string> WelcomeGreetingsResponse(string name)
         {
-            var greetings = new List<string> { "hi", "hello", "hey" };
-            return AppendName(greetings, name);
+            var greetings = new List<string> { "Thanks for contacting the IT ServiceDesk. How may I help you" };
+            return AppendNameQuestion(greetings, name);
         }
         public static List<string> GoodMorningGreetingsResponse(string name)
         {
             var greetings = GetGreeting();
             return AppendName(greetings, name);
+        }
+
+        public static List<string> PasswordResetResponse(string name)
+        {
+            var greetings = ResetPassword();
+            return AppendName(greetings, name);
+        }
+
+        public static List<string> UnlockAccountResponse(string name, string text)
+        {
+            //var greetings = AccountUnlock(text);
+            //return AppendName(greetings, name);
+            return new List<string> {
+                name
+            };
         }
 
         public static List<string> CallResponse()
@@ -41,11 +57,59 @@ namespace LyncBot.Core
                 return new List<string> { "Good Evening" };
         }
 
+        private static List<string> ResetPassword()
+        {
+            return new List<string>
+            {
+                "One moment. Let me try resetting your password",
+                "Resetting your password. Please wait",
+                "Okay. I'll reset your password"
+            };
+        }
+
+        private static List<string> AccountUnlock(string text)
+        {
+            if(Regex.IsMatch(text, "cpr"))
+            {
+                return new List<string>
+                {
+                 "I'll unlock your CPR+ account"
+                };
+            }
+            else if (Regex.IsMatch(text, "mckesson"))
+            {
+                return new List<string>
+                {
+                 "I'll unlock your McKesson account"
+                };
+            }
+            else
+            {
+                return new List<string>
+                {
+                    "I don't recognize that application: " + text
+                };
+            }
+
+        }
+
         private static List<string> AppendName(List<string> responses, string name)
         {
             if (!string.IsNullOrEmpty(name))
-                responses = responses.Concat(responses.Select(t => t + " " + name)).ToList();
+                responses = responses.Concat(responses.Select(t => t + ", " + name + ".")).ToList();
+            else
+                responses = responses.Concat(responses.Select(t => t + ".")).ToList();
             return responses;
         }
+        private static List<string> AppendNameQuestion(List<string> responses, string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+                responses = responses.Concat(responses.Select(t => t + ", " + name + "?")).ToList();
+            else
+                responses = responses.Concat(responses.Select(t => t + "?")).ToList();
+            return responses;
+        }
+
     }
+
 }
